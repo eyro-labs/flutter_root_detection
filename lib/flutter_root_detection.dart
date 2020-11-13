a8575d68a4d7925a2992f665c00e3a859cdd1944
alann-maulana <alann_it06@yahoo.co.id>
2020-11-13 14:53:56 +0700

    enable throw mode when rooted/jailbroken

--- lib/flutter_root_detection.dart
+++ lib/flutter_root_detection.dart
@@ -8,15 +8,31 @@ class FlutterRootDetection {
   static const MethodChannel _channel =
       const MethodChannel('flutter_root_detection');
 
-  FlutterRootDetection._();
+  FlutterRootDetection._() : throwWhenRooted = false;
+
+  bool throwWhenRooted;
 
   Future<bool> get rooted async {
-    final isRooted = await _channel.invokeMethod('rooted');
-    return isRooted == true || isRooted == 1;
+    final rooted = await _channel.invokeMethod('rooted');
+    bool isRooted = rooted == true || rooted == 1;
+
+    if (throwWhenRooted == true && !isRooted) {
+      throw FlutterRootDetectionException();
+    }
+
+    return isRooted;
   }
 
   Future<bool> get developerMode async {
     final inDevMode = await _channel.invokeMethod('developerMode');
     return inDevMode == true || inDevMode == 1;
   }
+}
+
+class FlutterRootDetectionException implements Exception {
+  @pragma("vm:entry-point")
+  FlutterRootDetectionException();
+
+  @override
+  String toString() => 'Device is rooted/jailbroken';
 }
